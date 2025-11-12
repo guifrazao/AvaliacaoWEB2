51c240e8456b58959f4f574148f39be898eed7fe
Kendy Outi <162067440+KendyOuti@users.noreply.github.com>
2025-11-12 20:19:59 -0300

    Add files via upload

---
+++ db.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql2');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Erro ao conectar ao banco de dados: ' + err.stack);
+        return;
+    }
+    console.log('Conectado ao MySQL com ID: ' + connection.threadId);
+});
+
+module.exports = connection.promise(); 
---
+++ session.js
@@ -0,0 +1,17 @@
+const session = require('express-session');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const sessionConfig = session({
+    secret: process.env.SESSION_SECRET, 
+    resave: false,
+    saveUninitialized: false, 
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24, 
+        secure: false, 
+        httpOnly: true, 
+        sameSite: false 
+    }
+});
+
+module.exports = sessionConfig;
