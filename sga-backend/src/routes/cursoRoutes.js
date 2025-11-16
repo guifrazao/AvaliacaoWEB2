bc60f194956bf1da7d3acf9c67a69a88ce7f6cf4
ChMoreiraa <163786986+ChMoreiraa@users.noreply.github.com>
2025-11-16 19:39:41 -0300

    Add files via upload

---
+++ sga-backend/src/routes/cursoRoutes.js
@@ -0,0 +1,26 @@
+// sga-backend/src/routes/cursoRoutes.js
+const express = require('express');
+const cursoController = require('../controllers/cursoController');
+const requireAuth = require('../middlewares/authMiddleWare'); // CORREÇÃO: Importa diretamente o valor exportado.
+
+const router = express.Router();
+
+// Todas as rotas de curso requerem autenticação
+router.use(requireAuth); 
+
+// Rota para listar todos os cursos (Read All)
+router.get('/', cursoController.findAll);
+
+// Rota para buscar um curso por ID (Read One)
+router.get('/:id', cursoController.findById); 
+
+// Rota para cadastrar um novo curso (Create)
+router.post('/', cursoController.create);
+
+// Rota para atualizar um curso por ID (Update)
+router.put('/:id', cursoController.update);
+
+// Rota para excluir um curso por ID (Delete)
+router.delete('/:id', cursoController.remove);
+
+module.exports = router;
